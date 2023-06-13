dfaf59763e7c7bebd0f271b29a0ba038a502f07f
ianodhiambo1 <ian.odhiambo@strathmore.edu>
2023-06-13 23:18:04 +0300

    Functional login/ basic homepage

--- login.php
+++ login.php
@@ -1,22 +1,21 @@
 <?php
 
 include 'config.php';
+session_start();
 
 if(isset($_POST['submit'])){
 
-   $name = mysqli_real_escape_string($conn, $_POST['name']);
    $email = mysqli_real_escape_string($conn, $_POST['email']);
    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
-   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
 
    $select = mysqli_query($conn, "SELECT * FROM `user_info` WHERE email = '$email' AND password = '$pass'") or die('query failed');
 
    if(mysqli_num_rows($select) > 0){
-      $message[] = 'user already exist!';
+      $row = mysqli_fetch_assoc($select);
+      $_SESSION['user_id'] = $row['id'];
+      header('location:index.php');
    }else{
-      mysqli_query($conn, "INSERT INTO `user_info`(name, email, password) VALUES('$name', '$email', '$pass')") or die('query failed');
-      $message[] = 'registered successfully!';
-      header('location:login.php');
+      $message[] = 'incorrect password or email!';
    }
 
 }
@@ -29,7 +28,7 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>register</title>
+   <title>login</title>
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="./style.css">
@@ -48,13 +47,11 @@ if(isset($message)){
 <div class="form-container">
 
    <form action="" method="post">
-      <h3>register now</h3>
-      <input type="text" name="name" required placeholder="enter username" class="box">
+      <h3>login now</h3>
       <input type="email" name="email" required placeholder="enter email" class="box">
       <input type="password" name="password" required placeholder="enter password" class="box">
-      <input type="password" name="cpassword" required placeholder="confirm password" class="box">
-      <input type="submit" name="submit" class="btn" value="register now">
-      <p>already have an account? <a href="login.php">login now</a></p>
+      <input type="submit" name="submit" class="btn" value="login now">
+      <p>don't have an account? <a href="register.php">register now</a></p>
    </form>
 
 </div>
