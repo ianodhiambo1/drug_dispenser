57547f3042e06055cca4a1567d67b6423fc99674
ianodhiambo1 <ian.odhiambo@strathmore.edu>
2023-06-18 11:49:00 +0300

    Rename

---
+++ DoctorLogin/Doctors.php
@@ -0,0 +1,8 @@
+<?php
+$sql = "SELECT * FROM patients#2";
+results = mysqli_query($conn,$sql);
+$reultsCheck = mysqli_num_rows($results);
+
+
+if (resultCheck > 0)
+>
--- Drug Dispensery/Doctors.php
+++
@@ -1,8 +0,0 @@
-<?php
-$sql = "SELECT * FROM patients#2";
-results = mysqli_query($conn,$sql);
-$reultsCheck = mysqli_num_rows($results);
-
-
-if (resultCheck > 0)
->
