daa84c821cf5307912ff307451c5cf4ea1e43f2c
ianodhiambo1 <ian.odhiambo@strathmore.edu>
2023-06-20 22:26:04 +0300

    redesign. beautiful landing page

--- UserLogin/index2.php
+++ UserLogin/index2.php
@@ -38,28 +38,23 @@ if(isset($message)){
 ?>
 
 <div class="container">
-<nav>
-		<input type="checkbox" id="check">
-		<label for="check" class="checkbtn">
-			<i class="fas fa-bars"></i>
-		</label>
-		<label class="logo">Store Logo</label>
-		<ul>
-      <?php
+<nav class="nav">
+   <div class="contain">
+   <img src="images/logo.svg" alt="Logo" class="siteLogo">
+   <?php
       $select_user = mysqli_query($conn, "SELECT * FROM `user_info` 
       WHERE id = '$user_id'") or die('query failed');
       if(mysqli_num_rows($select_user) > 0){
          $fetch_user = mysqli_fetch_assoc($select_user);
       };
    ?>
-			<li><a href="index2.php?logout=<?php echo $user_id; ?>" 
-         onclick="return confirm('are your sure you want to logout?');" 
-         class="logout">logout</a></li>
-         <img src="images/userLogo.png" alt="UserLogo" width="35px" 
-         style="padding-top: 10px;" >
-			<li><span><?php echo $fetch_user['name']; ?></span></li>
-		</ul>
-	</nav>
-
+   <div class="userInfo">
+      <a href="index.php?logout=<?php echo $user_id; ?>" 
+         onclick="return confirm('are your sure you want to logout?');" class="logOut">Log out</a>
+      <span><?php echo $fetch_user['name']; ?></span>
+   </div>
+   </div>
+   
+</nav>
 </body>
 </html>
